bac89cdda30ef699c9b83406e2600c0578fae11e
Connor Ott <44052532+evilcat311@users.noreply.github.com>
2023-09-11 15:08:07 -0400

    init repo

---
+++ DE1_SoC_Demo2/src/HW/nios_system/nios_systyem_hw.bat
@@ -0,0 +1,4 @@
+qsys-script --script=nios_systyem_hw.tcl
+qsys-generate nios_system.qsys --syn=VHDL
+REM C:\intelFPGA\18.1\quartus\sopc_builder\bin\qsys-script --script=nios_systyem_hw.tcl
+REM C:\intelFPGA\18.1\quartus\sopc_builder\bin\qsys-generate nios_system.qsys --syn=VHDL
